8d5d044d14956dc737821dc329a96e64a72ae945
Victoria Breen <victoriabreen8@hotmail.com>
2024-10-07 19:32:36 -0230

    Adding Data for Question 1

--- Question1/createTables.sql
+++ Question1/createTables.sql
@@ -1,5 +1,34 @@
 -- students
 
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL, -- text instead of varchar so you don't have to guess length
+    last_name TEXT NOT NULL, -- always want the columns to be filled out and not left empty
+    email TEXT NOT NULL,
+    enrollment_date DATE NOT NULL
+
+);
 -- professors
+CREATE TABLE professors (
+    professor_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    department TEXT NOT NULL
+);
 -- courses
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name TEXT NOT NULL,
+    course_description TEXT NOT NULL, 
+    professor_id INT,
+    FOREIGN KEY (professor_id) REFERENCES professors(professor_id)
+);
 -- enrollments 
+CREATE Table enrollments (
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    enrollment_date DATE NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
--- Question1/insertingData.sql
+++ Question1/insertingData.sql
@@ -1,3 +1,37 @@
 -- 5 students, 4 professors and 3 courses
 -- need at least 5 enrollments
 -- students in multiples courses
+
+-- insert for student tables 
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Chloe', 'Smith', 'chloe.smith@codemail.com', '2024-10-07'),
+('Dawson', 'Porter', 'dawson.porter@codemail.com', '2024-10-08'),
+('Bernard', 'Hunt', 'bernard.hunt@codemail.com', '2024-10-06'),
+('Harrison', 'Chippet', 'harrison.chippet@codemail.com', '2024-10-05'),
+('Victoria', 'Breen', 'victoria.breen@codemail.com', '2024-10-04');
+
+-- insert for professors tables
+INSERT INTO professors(first_name, last_name, department) VALUES
+('Matthew', 'English', 'DataBase'),
+('Mo', 'Babin', 'Python'),
+('Anie', 'Mailk', 'Java'),
+('Katelyn', 'Hoyles', 'Math');
+
+-- insert for courses table
+INSERT INTO courses(course_name, course_description, professor_id ) VALUES
+('Database', 'Introduction to Database', 1),
+('Java', 'Introduction to Java', 2),
+('Math', 'Calculus I and II', 3),
+('Python', 'Introduction to Python', 2),
+('FullStack Javascript', 'Introduction to Full-stack JavaScript ', 1);
+
+-- insert for enrollments table
+INSERT INTO enrollments(student_id, course_id, enrollment_date) VALUES
+(1, 1, '2024-11-05'),
+(2, 3, '2024-11-05'),
+(3, 2, '2024-11-05'),
+(4, 4, '2024-11-04'),
+(5, 1, '2024-11-04');
+
+
+
