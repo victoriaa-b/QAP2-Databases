63b79fb6ed33a7c0c2d8d01a5c3786de73d7ec9a
Victoria Breen <victoriabreen8@hotmail.com>
2024-10-08 16:55:56 -0230

    Added queries

--- Question1/tasks.sql
+++ Question1/tasks.sql
@@ -1,2 +1,25 @@
--- retreive full name, first and last joined
+-- retrieve full name, first and last joined
+SELECT CONCAT(students.first_name,' ', students.last_name) AS full_name
+FROM students 
+JOIN enrollments ON students.student_id = enrollments.student_id
+JOIN courses ON enrollments.course_id = courses.course_id
+WHERE courses.course_name = 'Java';
 
+-- retrieves all courses and profs full name
+SELECT courses.course_name, CONCAT(professors.first_name, ' ', professors.last_name) AS professor_name
+FROM courses
+JOIN professors ON courses.professor_id = professors.professor_id;
+
+-- retrieves all courses that has student in them
+SELECT DISTINCT courses.course_name
+FROM courses
+JOIN enrollments ON courses.course_id = enrollments.course_id;
+
+-- updates a student email
+UPDATE students
+SET email = 'dawson.porter@hotmail.com'
+WHERE students.student_id = 2;
+
+-- remove student from one of their courses 
+DELETE FROM enrollments
+WHERE enrollments.student_id = 2 AND course_id = 3;
